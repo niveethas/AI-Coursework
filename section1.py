87ea5ea96b2687be1f459e8f2f4adaf3912eb0e7
niveethas <61502706+niveethas@users.noreply.github.com>
2023-01-26 21:56:09 +0000

    Update section1.py
    fixing cosine, added api

--- section1.py
+++ section1.py
@@ -4,7 +4,9 @@ Created on Mon Jan  9 21:31:51 2023
 
 @author: nivee
 """
-#!pip install scikit-learn
+# !pip install aiml
+# !pip install wikipedia
+# !pip install scikit-learn
 import wikipedia
 import aiml
 import sklearn
@@ -13,6 +15,8 @@ from sklearn.metrics.pairwise import linear_kernel
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 import numpy as np
+import json, requests
+
 
 # Create a Kernel object. No string encoding (all I/O is unicode)
 kern = aiml.Kernel()
@@ -33,47 +37,67 @@ while True:
     #activate selected response agent
     if responseAgent == 'aiml':
         answer = kern.respond(userInput)
-        # --------- MAKE SURE TO CHANGE THE CODE IN HERE TO NOT PLAGARISE ---------'
-
-        df = pd.read_csv('QA.csv').dropna()
-       # makeupBot = df['Question']
-       # print (type(makeupBot))
-        r = [userInput]
-       # temp = pd.Series(r,copy=False)
-        #makeupBot = makeupBot.append(temp, ignore_index = True)
-        #print (makeupBot)
-        
-        #to find data type use type(variable-name)
-        
-
-        #https://goodboychan.github.io/python/datacamp/natural_language_processing/2020/07/17/04-TF-IDF-and-similarity-scores.html
-        # Create TfidfVectorizer object
-        vectorizer = TfidfVectorizer()
-
-        # Generate matrix of word vectors
-        #tfidf_matrix = vectorizer.fit_transform(makeupBot)
-
-        #https://stackoverflow.com/questions/58240401/matching-phrase-using-tf-idf-and-cosine-similarity
-        #this works
-        similarity_index_list = cosine_similarity(vectorizer.fit_transform(df["Question"]), vectorizer.transform(r)).flatten()
-        output = df.loc[similarity_index_list.argmax(), "Answer"]
-        print ("output",output)
-       
-        #consine_similarities = linear_kernel(tfidf_matrix[0:10],tfidf_matrix).flatten()
-        
-        #indices =  consine_similarities.argsort()[:-5:-1]
-       # print (indices)
-        #print(consine_similarities[indices])
-        #print (makeupBot)
-
-        # --------- MAKE SURE TO CHANGE THE CODE IN HERE TO NOT PLAGARISE ---------
         if answer[0] == '#':
             params = answer[1:].split('$')
             cmd = int(params[0])
             if cmd == 0:
                 print(params[1])
                 break
+            #else:
+            elif cmd == 1:
+                #what is maybelline, may be cmd 3 not 2 -> 2 is how is weather i think
+                    succeeded = False
+                    api_url = r"https://makeup-api.herokuapp.com/api/v1/products.json?"
+                    inputBrandProd = params[1]
+                    inputBrandProd = inputBrandProd.split(" ")
+                    response = requests.get(api_url + r"brand=" + inputBrandProd[0] + r"&product_type=" + inputBrandProd[1])
+                    if response.status_code == 200:
+                        response_json = json.loads(response.content)
+                        if response_json:
+                            name = response_json[0]['name']
+                            prod_type = response_json[0]['product_type']
+                            description = response_json[0]['description']
+                            print('\n\bName:\t' , name,'\n\n\bDescription:\n\t', description, "\n")
+                            succeeded = True
+                    if not succeeded:
+                        print("Sorry, I could not find an example for the brand and product you gave me")
             elif cmd == 99:
-                print("I did not get that, please try again.")
+                
+                try:
+                    # --------- MAKE SURE TO CHANGE THE CODE IN HERE TO NOT PLAGARISE ---------'
+                    df = pd.read_csv('QA.csv').dropna()
+                    #inputArray = userInput.split()
+                    inputArray = [userInput]
+                    counter = 0
+                    #https://goodboychan.github.io/python/datacamp/natural_language_processing/2020/07/17/04-TF-IDF-and-similarity-scores.html
+                    # Create TfidfVectorizer object
+                    vectorizer = TfidfVectorizer()
+
+                   
+                    #https://stackoverflow.com/questions/58240401/matching-phrase-using-tf-idf-and-cosine-similarity
+                    #for x in range(len(inputArray)):
+                     #   temp = [inputArray[x]]
+                      #  print (temp, " = temp")
+                    
+                    #LEMATISE THE QUESTIONS ARRAY FIRST    
+                
+                    similarity_index_list = cosine_similarity(vectorizer.fit_transform(df["Question"]), vectorizer.transform(inputArray)).flatten()
+                    output = df.loc[similarity_index_list.argmax(), "Answer"]
+                    
+                    for x in similarity_index_list:
+                        if x <= 0.1:
+                            counter = counter + 1
+
+                    #only printing the answer if there is a suitable similarity level       
+                    if counter == len(similarity_index_list):
+                            print ("I'm sorry, I don't have an answer for that.")
+                    else:
+                            print (output)
+                    
+                    # --------- MAKE SURE TO CHANGE THE CODE IN HERE TO NOT PLAGARISE ---------
+                except:    
+                    print("I did not get that, please try again.")
         else:
             print(answer)
+    else:
+        print ("aiml not needed")
